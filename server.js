683858a3b4fff24f7bb2ff0f8c4c09d966d2d26c
NguyenThaiHoc1 <thaihocmap123@gmail.com>
2017-04-10 12:10:51 +0700

    Scour File Master

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+
+
+
+
+// port  listen 8080
+app.listen(8080, function (err) {
+    if(err)
+      console.error(err);
+    console.log("Server is connecting in 8080");
+});
